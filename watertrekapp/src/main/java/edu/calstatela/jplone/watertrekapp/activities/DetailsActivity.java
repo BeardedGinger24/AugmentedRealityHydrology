da789fe7e78bd509b404b13e6a24c3d724f46aa2
Fugi <rarroyo8@calstatela.edu>
2018-09-21 12:52:38 -0700

    Added login credentials check, and enable/disable switch toggles

--- watertrekapp/src/main/java/edu/calstatela/jplone/watertrekapp/activities/DetailsActivity.java
+++ watertrekapp/src/main/java/edu/calstatela/jplone/watertrekapp/activities/DetailsActivity.java
@@ -19,12 +19,6 @@ public class DetailsActivity extends AppCompatActivity {
         String type = getIntent().getStringExtra("type");
 
         ImageView imgData = findViewById(R.id.img_data);
-//        if(type.equals("well"))
-//            imgData.setImageResource(R.drawable.well_color);
-//        else if(type.equals("reservoir"))
-//            imgData.setImageResource(R.drawable.reservoir_res_ico_clr);
-//        else if(type.equals("soil"))
-//            imgData.setImageResource(R.drawable.grass_res_ico_clr);
 
         switch(type){
             case "well":
