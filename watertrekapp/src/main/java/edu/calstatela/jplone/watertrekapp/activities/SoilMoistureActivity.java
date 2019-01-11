7a43268d4b56193f464144c70ed21d3b8fa2cbac
leonardoobay <leonardoobay@gmail.com>
2019-01-10 22:46:02 -0800

    Added SoilMoist need to fix image on map and add multiple images to map hardcoded location value need to add toast mssg when no soils nearby

---
+++ watertrekapp/src/main/java/edu/calstatela/jplone/watertrekapp/activities/SoilMoistureActivity.java
@@ -0,0 +1,71 @@
+package edu.calstatela.jplone.watertrekapp.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.OverlayItem;
+
+import java.util.ArrayList;
+
+
+import edu.calstatela.jplone.watertrekapp.Data.SoilMoisture;
+import edu.calstatela.jplone.watertrekapp.R;
+
+public class SoilMoistureActivity extends AppCompatActivity {
+    MapView map;
+    GeoPoint defaultLocation;
+    IMapController mapController;
+    ItemizedOverlayWithFocus<OverlayItem> mOverlay;
+    ArrayList<OverlayItem> markers;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        float lat = Float.parseFloat(getIntent().getStringExtra("lat"));
+        float lon = Float.parseFloat(getIntent().getStringExtra("lon"));
+        defaultLocation = new GeoPoint(lat,lon);
+        TextView txtData = findViewById(R.id.txt_data);
+        txtData.setText(getIntent().getStringExtra("data"));
+        map = findViewById(R.id.map);
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        mapController = map.getController();
+        mapController.setZoom(18);
+        mapController.setCenter(defaultLocation);
+        Marker marker = new Marker(map);
+        marker.setPosition(defaultLocation);
+        marker.setAnchor(Marker.ANCHOR_CENTER,Marker.ANCHOR_BOTTOM);
+        marker.setIcon(this.getDrawable(R.drawable.soil_bb_icon));
+        marker.setTitle("Soil Moisture: LatLon("+lat+","+lon+")");
+        map.getOverlays().add(marker);
+    }
+    public static void launchDetailsActivity(Activity currentActivity, SoilMoisture e) {
+        Log.d("LaunchSoildetails","going now...");
+        Intent intent = new Intent(currentActivity, SoilMoistureActivity.class);
+        intent.putExtra("data", e.toString());
+        intent.putExtra("lat",e.getLat());
+        intent.putExtra("lon",e.getLon());
+        currentActivity.startActivity(intent);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        map.onPause();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        map.onResume();
+    }
+}
