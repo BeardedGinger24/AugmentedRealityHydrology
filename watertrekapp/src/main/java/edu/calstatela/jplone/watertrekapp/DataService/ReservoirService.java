117004d0a1fc703b48057911da5209fa5bec7190
leonardoobay <leonardoobay@gmail.com>
2019-03-30 00:11:11 -0700

    reservoirs load time improved with GSON

--- watertrekapp/src/main/java/edu/calstatela/jplone/watertrekapp/DataService/ReservoirService.java
+++ watertrekapp/src/main/java/edu/calstatela/jplone/watertrekapp/DataService/ReservoirService.java
@@ -3,14 +3,19 @@ package edu.calstatela.jplone.watertrekapp.DataService;
 
 import android.util.Log;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
 import edu.calstatela.jplone.watertrekapp.Data.Reservoir;
+import edu.calstatela.jplone.watertrekapp.Data.ReservoirStorageData;
 import edu.calstatela.jplone.watertrekapp.Data.River;
 import edu.calstatela.jplone.watertrekapp.NetworkUtils.NetworkTask;
 import edu.calstatela.jplone.watertrekapp.NetworkUtils.NetworkTaskAuth;
@@ -166,33 +171,27 @@ static String TAG = "reservoir-service";
 
     }
     public static List parseStoragesJSON(String line){
+        Gson gson = new Gson();
+
+
+//        usgs_reservoir_data usgrd = gson.fromJson(line,usgs_reservoir_data.class);
+
+
+        // {"usgs_reservoir_data":[{"datetime":"1981-09-01T00:00:00","storage":2100.0,"units":"ac-ft"},
+        // {"datetime":"1981-09-02T00:00:00","storage":1970.0,"units":"ac-ft"},
         List<String> storeList = new ArrayList();
         try {
-            JSONObject results = new JSONObject(line);
-            JSONArray accessRESERVOIR = results.getJSONArray("usgs_reservoir_data");
-            int length = accessRESERVOIR .length();
-            for (int x =0 ; x<length; x++)
-            {
-                //keys are   {"datetime":"1981-09-01T00:00:00","discharge":4.870498,"units":"m^3/s"}
 
-                JSONObject jsondateTime = accessRESERVOIR.getJSONObject(x);
-                JSONObject jsonStore = accessRESERVOIR.getJSONObject(x);
-                JSONObject jsonUnits = accessRESERVOIR.getJSONObject(x);
-//                String dateTime = "004-034556";
-                String[] parts = jsondateTime.getString("datetime").split("T");
-                String resDateOnly = parts[0];
-                String resTimeOnly = parts[1];
-                String storeAmount = jsonStore.getString("storage");
-                String resUnits = jsonUnits.getString("units");
-                // Two spaces are used inbetween units
-                String  resOutput = resDateOnly+"  "+storeAmount+"  "+resUnits;
-//                Log.d("JSONRIVER", jsondate.getString("datetime"));
-//                Log.d("JSONRIVER", jsonDis.getString("discharge"));
-//                Log.d("JSONRIVER", jsonUnits.getString("units"));
-                Log.d("JSONRESERVOIR", resOutput);
-                storeList.add(resOutput);
-            }
-            return storeList;
+            JSONObject results = new JSONObject(line);
+            String   reservoirString =   results.toString();
+            JSONArray accessRESERVOIR = results.getJSONArray("usgs_reservoir_data");
+            String resArray = accessRESERVOIR.toString();
+            Type ResStorageList = new TypeToken<ArrayList<ReservoirStorageData>>(){}.getType();
+           // ReservoirStorageData[] rsd = new Gson().fromJson(resArray,ReservoirStorageData[].class);
+            List<ReservoirStorageData> resDlist = new Gson().fromJson(resArray,ResStorageList);
+
+            return resDlist;
+//            return storeList;
         }catch (JSONException e) {
             Log.d("JSONRESERVOIR", "error  happened");
             return storeList;
--- watertrekapp/src/main/java/edu/calstatela/jplone/watertrekapp/activities/HistoryActivity.java
+++ watertrekapp/src/main/java/edu/calstatela/jplone/watertrekapp/activities/HistoryActivity.java
@@ -34,6 +34,7 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 
+import edu.calstatela.jplone.watertrekapp.Data.ReservoirStorageData;
 import edu.calstatela.jplone.watertrekapp.DataService.ReservoirService;
 import edu.calstatela.jplone.watertrekapp.DataService.RiverService;
 import edu.calstatela.jplone.watertrekapp.DataService.SnotelService;
@@ -667,9 +668,8 @@ public class HistoryActivity extends AppCompatActivity implements DatePickerDial
     NetworkTaskAuth.NetworkCallback reservoirNetworkCallbackJSON = new NetworkTaskAuth.NetworkCallback() {
         @Override
         public void onResult(int type, String result) {
-            List<String> strgeList = ReservoirService.parseStoragesJSON(result);
-
-            if (strgeList.size() < 1) {
+            List <ReservoirStorageData> resResults = ReservoirService.parseStoragesJSON(result);
+            if (resResults.size() < 1) {
                 Log.d("Reservoir-Storage", "No information has been recorded thus far");
                 pb.setVisibility(View.INVISIBLE);
                 getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
@@ -682,28 +682,25 @@ public class HistoryActivity extends AppCompatActivity implements DatePickerDial
                 resStorageList.clear();
                 yValue.clear();
                 xValue.clear();
-                for (int i = 0; i < strgeList.size(); i++) {
-                    String dsl = strgeList.get(i);
-                    resStorageList.add(dsl);
-                    if (i > 0) {
-                        String[] date = dsl.split("  ");
-                        // values are seperated by tabs not spaces.
-                        String[] value = date[1].split("  ");
-                        xValue.add(date[0]);
-                        yValue.add(value[0]);
-                        Log.i("x-value", xValue.get(i - 1));
-                        Log.i("y-value", yValue.get(i - 1));
-                    }
+                for (int i = 0; i < resResults.size(); i++) {
+                    String tempDT = resResults.get(i).getDateTime();
+                    String tempStorage = resResults.get(i).getStorage();
+                    String tempUnit = resResults.get(i).getUnits();
+                    String tempLast = tempDT +" " + tempStorage + " " + tempUnit;
+                    resStorageList.add(tempLast);
+//
+//                    if (i > 0) {
+//                        String[] date = dsl.split("  ");
+//                        // values are seperated by tabs not spaces.
+//                        String[] value = date[1].split("  ");
+//                        xValue.add(date[0]);
+//                        yValue.add(value[0]);
+//                        Log.i("x-value", xValue.get(i - 1));
+//                        Log.i("y-value", yValue.get(i - 1));
+//                    }
                 }
-                // Check if all y values are null before populating graph
-                if (allValuesNull(yValue)) {
-                    Log.i("allYNull", "All Y values are null");
-                    Toast.makeText(getApplicationContext(), "All values are null.", Toast.LENGTH_LONG).show();
-                    pb.setVisibility(View.INVISIBLE);
-                    getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
-                    return;
-                }
-                populateGraph(xValue, yValue);
+
+//                populateGraph(xValue, yValue);
                 pb.setVisibility(View.INVISIBLE);
                 ListView lv = findViewById(R.id.historyList);
                 ArrayAdapter<String> adapter = new ArrayAdapter<String>(HistoryActivity.this, android.R.layout.simple_list_item_1, resStorageList);
@@ -712,7 +709,6 @@ public class HistoryActivity extends AppCompatActivity implements DatePickerDial
             }
         }
     };
-
 
     //*****************RESRVOIR RECYCLER VIEW JSON ENDS **********************************************
 
